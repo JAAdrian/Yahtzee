f92548350e10199fc3939c986e541d08039e442f
Jens-Alrik Adrian <adrian@kaigo.de>
2026-07-06 12:14:53 +0200

    Streamline finish flow

--- games/tests.py
+++ games/tests.py
@@ -201,6 +201,16 @@ class GameViewTests(TestCase):
         game.refresh_from_db()
         self.assertTrue(game.is_complete)
 
+    def test_finish_game_accepts_get_with_confirmation(self):
+        game = Game.objects.create()
+        response = self.client.get(reverse("game_finish", kwargs={"pk": game.pk}))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(
+            response.url.endswith(reverse("game_results", kwargs={"pk": game.pk}))
+        )
+        game.refresh_from_db()
+        self.assertTrue(game.is_complete)
+
     def test_delete_game(self):
         game = Game.objects.create()
         response = self.client.post(reverse("game_delete", kwargs={"pk": game.pk}))
